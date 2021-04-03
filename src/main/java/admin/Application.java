1c07187321b5d254bf2782b950078cb926a9e3c8
myunghoonju <myunghoonju@kr.doubledown.com>
2021-04-04 01:17:22 +0900

    no message
    (cherry picked from commit f57331f8c1912a813b1712c46571af8b1d7ce739)

--- src/main/java/admin/Application.java
+++ src/main/java/admin/Application.java
@@ -2,7 +2,7 @@ package admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// https://jojoldu.tistory.com/539
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
