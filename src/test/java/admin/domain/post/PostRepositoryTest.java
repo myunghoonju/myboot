fdc4f0ce3bfb7751a03654b9a3b25d8e9d878fa8
JU_MYUNGHOON <myunghoonju@gmail.com>
2021-07-03 23:17:11 +0900

    생성/수정일짜 자동등록 테스트 추가.

--- src/test/java/admin/domain/post/PostRepositoryTest.java
+++ src/test/java/admin/domain/post/PostRepositoryTest.java
@@ -2,16 +2,18 @@ package admin.domain.post;
 
 import org.junit.After;
 import org.junit.Assert;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest // user h2
+@SpringBootTest // use h2
 public class PostRepositoryTest {
 
     @Autowired
@@ -39,4 +41,20 @@ public class PostRepositoryTest {
         Assert.assertEquals(post.getContent(), content);
     }
 
+    @Test
+    public void BaseTimeEntity_save() {
+        LocalDateTime now = LocalDateTime.of(2021, 7, 3, 0, 0, 0);
+        postRepository.save(Post.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        List<Post> postList = postRepository.findAll();
+        Post post = postList.get(0);
+
+        System.out.println(">>> createdDate:: " + post.getCreatedDate() + ", modifiedDate:: " + post.getModifiedDate());
+        assertThat(post.getCreatedDate()).isAfter(now);
+        assertThat(post.getModifiedDate()).isAfter(now);
+    }
 }
