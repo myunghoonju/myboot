e96d3ce2d4dc878e84807282ccbd49867f552d80
JU_MYUNGHOON <myunghoonju@gmail.com>
2021-07-03 22:48:50 +0900

    수정 테스트 추가. (dirty read 테스트)

--- src/test/java/admin/web/PostApiControllerTest.java
+++ src/test/java/admin/web/PostApiControllerTest.java
@@ -14,15 +14,18 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import admin.domain.post.Post;
 import admin.domain.post.PostRepository;
 import admin.web.dto.PostSaveRequestDto;
+import admin.web.dto.PostUpdateRequestDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,12 +50,12 @@ public class PostApiControllerTest {
 	}
 
 	@Test
-	public void post_등록_테스트() throws Exception {
+	public void add_post_test() throws Exception {
 		String title = "title_test2";
 		String content = "content_test2";
 		String author = "JMH";
-
 		String url = "http://localhost:" + port + "/api/v1/post";
+
 		PostSaveRequestDto postSaveRequestDto = PostSaveRequestDto.builder()
 			.title(title)
 			.content(content)
@@ -68,5 +71,37 @@ public class PostApiControllerTest {
 		assertThat(allPost.get(0).getTitle()).isEqualTo(title);
 		assertThat(allPost.get(0).getContent()).isEqualTo(content);
 		assertThat(allPost.get(0).getAuthor()).isEqualTo(author);
+	}
+
+	@Test
+	public void edit_post_test() throws Exception {
+		String title = "title_test3";
+		String content = "content_test3";
+		String author = "JMH";
+
+		Post newpost = postRepository.save(Post.builder()
+		.title(title)
+		.content(content)
+		.author(author)
+		.build());
+
+		Long updateId = newpost.getId();
+		String expectedTitle = "title_test4";
+		String expectedContent = "content_test4";
+
+		PostUpdateRequestDto postUpdateRequestDto = PostUpdateRequestDto.builder()
+			.title(expectedTitle)
+			.content(expectedContent)
+			.build();
+
+		String url = "http://localhost:" + port + "/api/v1/post/" + updateId;
+		mockMvc.perform(put(url)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(new ObjectMapper().writeValueAsString(postUpdateRequestDto))
+		).andExpect(status().isOk());
+
+		Post post = postRepository.findById(newpost.getId()).orElse(Post.builder().build());
+		assertThat(post.getTitle()).isEqualTo(expectedTitle);
+		assertThat(post.getContent()).isEqualTo(expectedContent);
 	}
 }
