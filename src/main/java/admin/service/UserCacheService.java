3efa3238819a0acbe36efd93bf0c3f9e1cb24f43
JU MYUNGHOON <myunghoonju@gmail.com>
2022-03-12 00:46:47 +0900

    add cache test 22

--- src/main/java/admin/service/UserCacheService.java
+++ src/main/java/admin/service/UserCacheService.java
@@ -3,6 +3,8 @@ package admin.service;
 import admin.config.auth.custom.annotation.LoginUser;
 import admin.config.auth.dto.SessionUser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,18 @@ public class UserCacheService {
     @Cacheable(value = "name", key = "#user.getName()")
     public String getUserName(@LoginUser SessionUser user) {
         log.info("UserCacheService.getUserName()");
+        return user.getName();
+    }
+
+    @CacheEvict(value="name", allEntries=true)
+    public String getUserNameCacheEvict(@LoginUser SessionUser user) {
+        log.info("UserCacheService.getUserNameCacheEvict()");
+        return user.getName();
+    }
+
+    @CachePut(value = "name", key = "#user.getName()")
+    public String putUserName(@LoginUser SessionUser user) {
+        log.info("UserCacheService.putUserName()");
         return user.getName();
     }
 }
--- src/test/java/admin/service/UserCacheServiceTest.java
+++ src/test/java/admin/service/UserCacheServiceTest.java
@@ -6,7 +6,6 @@ import admin.domain.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -49,5 +48,31 @@ public class UserCacheServiceTest {
         log.info("-----------------------");
     }
 
+    @Test
+    public void evictionTest() {
+        User a = User.builder().name("a").email("a@mail.com").picture("a.jpg").build();
+        SessionUser sessionUser = new SessionUser(a);
 
+        service.getUserName(sessionUser);
+        service.getUserNameCacheEvict(sessionUser);
+        log.info("-----------------------");
+
+        service.getUserName(sessionUser);
+        service.getUserName(sessionUser);
+        log.info("-----------------------");
+    }
+
+    @Test
+    public void putTest() {
+        User a = User.builder().name("a").email("a@mail.com").picture("a.jpg").build();
+        SessionUser sessionUser = new SessionUser(a);
+
+        service.putUserName(sessionUser);
+        service.getUserNameCacheEvict(sessionUser);
+        log.info("-----------------------");
+
+        service.putUserName(sessionUser);
+        service.putUserName(sessionUser);
+        log.info("-----------------------");
+    }
 }
