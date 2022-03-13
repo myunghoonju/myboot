fc3da27d68fcf0ef3afc79c4b1101e8ae6b60c90
JU MYUNGHOON <myunghoonju@gmail.com>
2022-03-14 02:14:11 +0900

    bdd start

--- src/test/java/admin/service/UserCacheServiceTest.java
+++ src/test/java/admin/service/UserCacheServiceTest.java
@@ -6,6 +6,7 @@ import admin.domain.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
