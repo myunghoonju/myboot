bd284153173d6d2ff66b65279518b1ce62f9b4b0
MYUNGHOON JU <myunghoonju@gmail.com>
2022-12-31 14:33:26 +0900

    init test

--- src/main/java/admin/rabbit/RabbitController.java
+++ src/main/java/admin/rabbit/RabbitController.java
@@ -1,6 +1,5 @@
 package admin.rabbit;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RabbitController {
 
-    private final QueueSender sender;
     private final RabbitTemplate template;
-/*
 
     @GetMapping("/test")
     public String send() {
-        sender.send("send test message from controller");
-        return "ok";
-    }
-*/
-
-
-    @GetMapping("/test")
-    public String send() throws JsonProcessingException {
         template.convertAndSend("topic-exchange", "test-routing.key", "121212");
         return "ok. done";
     }
