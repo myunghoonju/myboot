6eea3b7452c0d281f616faf540d49838b22c2ef3
MYUNGHOON JU <myunghoonju@gmail.com>
2023-01-09 23:37:32 +0900

    tidy up
    amqp

--- src/main/java/admin/rabbit/template/RabbitFTemplateWrapper.java
+++ src/main/java/admin/rabbit/template/RabbitFTemplateWrapper.java
@@ -1,10 +1,12 @@
 package admin.rabbit.template;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import static admin.config.amqp.exchange.TopicExchangeConfig.TOPIC_EXCHANGE_NAME;
 
+@Slf4j
 @Service
 public class RabbitFTemplateWrapper {
 
@@ -15,11 +17,15 @@ public class RabbitFTemplateWrapper {
     }
 
     public void sendMsg(String key, String msg) {
-        Boolean isSent = firstRabbit.invoke(ops -> {
+        boolean isSent = sendAndConfirm(key, msg);
+        log.info("sent result {}", isSent);
+    }
+
+    private boolean sendAndConfirm(String key, String msg) {
+        return Boolean.TRUE.equals(firstRabbit.invoke(ops -> {
             ops.convertAndSend(TOPIC_EXCHANGE_NAME, key, msg);
             return ops.waitForConfirms(1000);
-        });
-
-        System.out.println("SENT RESULT:: " + isSent);
+        }));
     }
+
 }
