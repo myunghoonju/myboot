3391a0003840f3f3a1beebd2bbca50865fb368df
MYUNGHOON JU <myunghoonju@gmail.com>
2023-01-09 23:48:20 +0900

    add try catch
    timeout

--- src/main/java/admin/rabbit/template/RabbitFTemplateWrapper.java
+++ src/main/java/admin/rabbit/template/RabbitFTemplateWrapper.java
@@ -1,10 +1,14 @@
 package admin.rabbit.template;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpTimeoutException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeoutException;
+
 import static admin.config.amqp.exchange.TopicExchangeConfig.TOPIC_EXCHANGE_NAME;
+import static java.lang.Boolean.TRUE;
 
 @Slf4j
 @Service
@@ -17,15 +21,21 @@ public class RabbitFTemplateWrapper {
     }
 
     public void sendMsg(String key, String msg) {
-        boolean isSent = sendAndConfirm(key, msg);
-        log.info("sent result {}", isSent);
+        try {
+            boolean isSent = sendAndConfirm(key, msg);
+            log.info("sent result {}", isSent);
+        } catch (AmqpTimeoutException e) {
+            log.error("sent failed {}", e.getMessage());
+        }
+
     }
 
     private boolean sendAndConfirm(String key, String msg) {
-        return Boolean.TRUE.equals(firstRabbit.invoke(ops -> {
-            ops.convertAndSend(TOPIC_EXCHANGE_NAME, key, msg);
-            return ops.waitForConfirms(1000);
-        }));
+        return TRUE.equals(
+                firstRabbit.invoke(ops -> {
+                    ops.convertAndSend(TOPIC_EXCHANGE_NAME, key, msg);
+                    return ops.waitForConfirms(1000);
+                })
+        );
     }
-
 }
