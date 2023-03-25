1f1f4080432a9dc80c05395560be1c47a6bb81b4
MYUNGHOON JU <myunghoonju@gmail.com>
2023-03-25 18:09:03 +0900

    remove vulnerabilities
    edit dependencies
     - swagger

--- src/main/java/admin/Application.java
+++ src/main/java/admin/Application.java
@@ -1,12 +1,10 @@
 package admin;
 
-import admin.config.property.ConfigEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Slf4j
@@ -17,9 +15,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        log.error("---redis host :: {}", ConfigEnum.REDIS_INFO.getHost());
-        log.error("---redis host :: {}", ConfigEnum.REDIS_INFO.getPort());
+        SpringApplication.run(Application.class, args);
     }
 }
