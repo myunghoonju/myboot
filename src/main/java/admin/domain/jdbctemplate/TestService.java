1b102a4914464e77ea197f4ced7a41aef5779adb
MYUNGHOON JU <myunghoonju@gmail.com>
2024-02-16 02:06:36 +0900

    empty param

--- src/main/java/admin/domain/jdbctemplate/TestService.java
+++ src/main/java/admin/domain/jdbctemplate/TestService.java
@@ -3,8 +3,10 @@ package admin.domain.jdbctemplate;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +26,13 @@ public class TestService {
     public String post(long id, String pa) {
         Ans ans1 = new Ans();
         ans1.setId(id);
-        String s = "select id from post partition(%s) where id = :id";
+        String s = "select id from post partition(%s) where id = 11";
         String formatted = String.format(s, pa);
-        Ans ans = (Ans) template.queryForObject(formatted, new BeanPropertySqlParameterSource(ans1), new Row());
+        Ans ans = template.queryForObject(formatted, EmptySqlParameterSource.INSTANCE, BeanPropertyRowMapper.newInstance(Ans.class));
         return ans.getId().toString();
     }
 
+    // replaced by BeanPropertyRowMapper.newInstance()
     class Row implements RowMapper {
         @Override
         public Ans mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -41,7 +44,7 @@ public class TestService {
 
     @Getter @Setter
     @NoArgsConstructor
-    class Ans {
+    static class Ans {
         private Long id;
         private String p;
     }
